719c0adbf41e65c667e7f1a0139b7e39d916899f
ttjahjadi93 <thomasTjahjadi93@gmail.com>
2018-09-07 17:05:09 +1000

    Made the hamburger menu

---
+++ html/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () { 
+ })
+
+
+function clickNav() {
+    document.querySelector('.button-container').classList.toggle('active');
+    document.querySelector('#overlay').classList.toggle('open');
+}
+
+
+
